7a6db8a9fde6aba1c089c4719414d0f13e071956
Remington Campbell <code@remington.io>
2023-03-10 07:45:24 -0800

    Add sqitch to setup script

--- setup_macos.sh
+++ setup_macos.sh
@@ -2,5 +2,7 @@
 brew install podman
 brew install libpq
 brew link --force libpq
+brew tap sqitchers/sqitch
+brew install sqitch --with-postgres-support
 # Share /Users/user to podman machine for bind mounts
 podman machine init --now --cpus=1 --memory=2048 -v $HOME:$HOME
